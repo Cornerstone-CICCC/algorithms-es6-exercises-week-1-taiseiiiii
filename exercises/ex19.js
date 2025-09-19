0a1b0151b7f3c1fc0f4a32fd54856fd85d804518
taiseiiiii <isemiya.0509@gmail.com>
2025-09-18 20:50:26 -0700

    exercise 19 completed

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -4,3 +4,20 @@ Call the emotions() function so that it prints the output you see below, but ins
 emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
 Prints: "I am happy, haha!"
 */
+
+function emotions(cnt, str, callback) {
+  if (isNaN(cnt)) {
+    console.log("Please input number");
+    return;
+  }
+  console.log(`I am ${str}, ${callback(cnt)}`);
+}
+
+emotions(5, "happy", (cnt) => {
+  let res = "";
+  for (let i = 0; i < cnt; i++) {
+    res += "ha";
+  }
+  res += "!";
+  return res;
+});
